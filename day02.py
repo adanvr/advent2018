30d516878b16896f08c92cf7c84825f407d895bb
Adan Romero <Adan.V.Romero@kp.org>
2018-12-02 12:41:52 -0800

    Updated count_doubles and count_triples to one liners for clarity, yarraimean?

--- day02.py
+++ day02.py
@@ -13,28 +13,20 @@ assert count_letters('aabbcc') == {'a':2, 'b':2, 'c':2}
 assert count_letters('ddff') == {'d':2, 'f':2}
 
 def count_doubles(data_dict):
-    if len({k: v for k, v in data_dict.items() if v == 2}) > 0:
-        return 1
-    else: 
-        return 0
+    return 1 if len({k: v for k, v in data_dict.items() if v == 2}) > 0 else 0
 
 assert count_doubles({'a':2, 'b':2, 'c':2}) == 1
 
 def count_triples(data_dict):
-    if len({k: v for k, v in data_dict.items() if v == 3}) > 0:
-        return 1
-    else: 
-        return 0
+    return 1 if len({k: v for k, v in data_dict.items() if v == 3}) > 0 else 0
 
 assert count_triples({'a':2, 'b':2, 'c':2}) == 0
 
-data_dict = [count_letters(datum) for datum in data]
-
-
-def check_sum(data_dict):
+def check_sum(data):
+    data_dict = [count_letters(datum) for datum in data]
     doubles = sum([count_doubles(datum) for datum in data_dict])
     triples = sum([count_triples(datum) for datum in data_dict])
     print(doubles * triples)
 
-check_sum(data_dict)
+check_sum(data)
 
