6ac947591103832f60b6dc3e7275af32446672fe
Adan Romero <Adan.V.Romero@kp.org>
2018-12-02 12:29:37 -0800

    Added some unit tests

--- day02.py
+++ day02.py
@@ -9,11 +9,16 @@ def count_letters(word_str):
     '''
     return dict(Counter(word_str))
 
+assert count_letters('aabbcc') == {'a':2, 'b':2, 'c':2}
+assert count_letters('ddff') == {'d':2, 'f':2}
+
 def count_doubles(data_dict):
     if len({k: v for k, v in data_dict.items() if v == 2}) > 0:
         return 1
     else: 
         return 0
+
+assert count_doubles({'a':2, 'b':2, 'c':2}) == 1
 
 def count_triples(data_dict):
     if len({k: v for k, v in data_dict.items() if v == 3}) > 0:
@@ -21,9 +26,10 @@ def count_triples(data_dict):
     else: 
         return 0
 
+assert count_triples({'a':2, 'b':2, 'c':2}) == 0
+
 data_dict = [count_letters(datum) for datum in data]
 
-#score_data = [count_doubles(datum) for datum in data_dict]
 
 def check_sum(data_dict):
     doubles = sum([count_doubles(datum) for datum in data_dict])
